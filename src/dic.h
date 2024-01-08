b330bc25f42af8b706685165fe00e6863846c982
Merlode11 <clementdiagallo@gmail.com>
2024-01-08 16:42:08 +0100

    Modifications sur la documentation

--- src/dic.h
+++ src/dic.h
@@ -15,6 +15,14 @@ typedef struct lettre_s* dico;
  * Crée un dictionnaire en initialisant ses valeurs de base ; lettre = true, et tous ses successeurs sont à false.
  */
 dico dictionnaire_vide(void);
+
+/**
+ * Rajoute un mot dans le dictionnaire
+ * @param dic
+ * @param mot
+ */
+void ajouter_mot(dico dic, char* mot);
+
 /**
  * Recherche un mot dans le dictionnaire, et renvoie son sous-dictionnaire associé, si il éxiste, sinon, renvoie NULL
  * @param dic Dictionnaire dans lequel chercher
--- src/main.c
+++ src/main.c
@@ -4,13 +4,30 @@
 
 int main(int argc, char* argv[])
 {
-	if(argc == 1)
+	/*if(argc == 1)
 	{
 		exit(EXIT_FAILURE);
-	}
+	}*/
 	
 	dico dic = dictionnaire_vide();
-    fopen("fr-reforme1990_court_sans.dic", "r");
-	
-	exit(EXIT_SUCCESS);
+    FILE* fd = fopen("./fr-reforme1990_court_sans.dic", "r");
+    if (fd == NULL) {
+        printf("Erreur lors de l'ouverture du fichier fr-reforme1990_court_sans.dic");
+        return 1;
+    }
+    int nb_mots = 0;
+    fscanf(fd, "%d", &nb_mots);
+
+    char string[100];
+    char poubelle[100];
+
+    fscanf(fd, "-");
+    for (int i = 0; i < nb_mots; ++i) {
+        fscanf(fd, "%s/%s", string, poubelle);
+        ajouter_mot(dic, string);
+        printf("%s\n", string);
+    }
+    fclose(fd);
+
+	return 0;
 }
