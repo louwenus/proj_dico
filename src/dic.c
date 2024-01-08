dc3e4ade7a89e2918cb44cc30c8e2b07dd8b7c3e
louwenus <louwen.fricout@gmail.com>
2024-01-08 16:22:31 +0100

    should work

--- src/dic.c
+++ src/dic.c
@@ -7,21 +7,20 @@ dico creer_dico()
 {
 	dico tmp = (dico)malloc(sizeof(struct lettre_s));
 	tmp->lettre = false;
-	for(int i=0; i<256; i++)
+	for(int i=0; i<26; i++)
 	{
 		(tmp->succ)[i] = NULL;
 	}
 	return tmp;
 }
 
-void ajouter_mot(dico dic, char* mot)  // Non fini. Problemes avec le NULL, possibles erreurs. normalement creer_dico() marche. Désolé <3
-{
+void ajouter_mot(dico dic, char* mot){
 	dico tmp = dic;
 	int i = 0;
 	while(mot[i] != '\0')
 	{
-		int succ_a_changer = mot[i] - 'a';  //ici on obtient un int de 0 a 25
-		if((tmp->succ)[succ_a_changer]->lettre == true)
+		int succ_a_changer = mot[i]-'a';
+		if((tmp->succ)[succ_a_changer] != NULL)
 		{
 			tmp = (tmp->succ)[succ_a_changer];
 		}
@@ -30,6 +29,26 @@ void ajouter_mot(dico dic, char* mot)  // Non fini. Problemes avec le NULL, poss
 			(tmp->succ)[succ_a_changer] = creer_dico();
 			tmp = (tmp->succ)[succ_a_changer];
 		}
+	i++;
 	}
-	tmp->succ[26] = true;
+	tmp->lettre = true;
+}
+
+dico chercher(dico dic,char* mot){
+	dico tmp = dic;
+	int i = 0;
+	while(mot[i] != '\0')
+	{
+		int succ = mot[i]-'a';
+		if((tmp->succ)[succ] != NULL)
+		{
+			tmp = (tmp->succ)[succ];
+		}
+		else
+		{
+			return NULL;
+		}
+	i++;
+	}
+	return tmp;
 }
--- src/dic.h
+++ src/dic.h
@@ -3,22 +3,25 @@
 #ifndef DIC_H
 #define DIC_H
 
+
+
 struct lettre_s {
 	bool lettre;
-	struct lettre_s* succ[256];
+	struct lettre_s* succ[26];
 };
 typedef struct lettre_s* dico;
 
 /*
  * Crée un dictionnaire en initialisant ses valeurs de base ; lettre = true, et tous ses successeurs sont à false.
  */
-dico creer_dico();
+dico creer_dico(void);
 
 /*
  * Ajoute un mot au dictionnaire en créant les structures nécessaires.
- * Entrees : dic, dictionnaire ENTIER.
+ * Entrees : dic, dictionnaire (pointeur).
  * 			 mot, chaine de caractère, mot à ajouter.
  */
 void ajouter_mot(dico dic, char* mot);
-
+//renvoie le sous-dictionnaire associé a un mot, si il existe, NULL sinon
+dico chercher(dico dic,char* mot);
 #endif
