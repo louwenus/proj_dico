8a401568bd910bf867ffb5d97ccadf072b3bbdd4
MoriartyHolmes <87190177+MoriartyHolmes@users.noreply.github.com>
2024-01-15 13:33:10 +0100

    Add files via upload

--- est_verbe.c
+++ est_verbe.c
@@ -1,2 +1,11 @@
-#include <stdhio>
+#include <stdio.h>
 #include "fr-reforme1990_court_sans.dic"
+#include <stdbool.h>
+
+void est_verbe(v) {
+    while(v[i]=!'/')
+    {
+        i++;
+    }
+    return v[i]>='a';
+}
