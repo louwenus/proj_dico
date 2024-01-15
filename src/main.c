6c89dd26ce28a439c7589b3e02059d99c088086a
louwenus <louwen.fricout@gmail.com>
2024-01-15 14:29:48 +0100

    reformating

--- src/main.c
+++ src/main.c
@@ -5,28 +5,31 @@
 #include "dic.h"
 #include "assert.h"
 
-int main(int argc, char* argv[])
-{
-  assert(argc == 2);
-	
-	dico dic = dictionnaire_vide();
+dico init_dico(void){
+
+    dico dic = dictionnaire_vide();
     FILE* fd = fopen("./fr-reforme1990_court_sans.dic", "r");
     if (fd == NULL) {
         printf("Erreur lors de l'ouverture du fichier fr-reforme1990_court_sans.dic");
-        return 1;
+        exit(1);
     }
     int nb_mots = 0;
     fscanf(fd, "%d\n", &nb_mots);
 
     char string[100];
-    char poubelle[100];
-
-    fscanf(fd, "%s\n", poubelle);
+    fscanf(fd, "%s\n", string);
     while (fscanf(fd, "%s\n", string) == 1) {
         ajouter_mot(dic, string);
     }
     fclose(fd);
+    return dic; 
+}
 
+int main(int argc, char* argv[])
+{
+  assert(argc == 2);
+
+	dico dic = init_dico();	
     FILE* input = fopen(argv[1], "r");
     errno = 0;
     if (input == NULL) {
@@ -36,6 +39,10 @@ int main(int argc, char* argv[])
 
     FILE *output;
     output = fopen("a_corriger.txt", "w");
+
+    
+   char string[100];
+    char poubelle[100];
 
     while (fscanf(input, "%s\n", string) == 1) {
         dico result=chercher_mot(dic,string );
