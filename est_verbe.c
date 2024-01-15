1b4c6e2347c73a7798d43bce5cf06a1f6319e8de
MoriartyHolmes <87190177+MoriartyHolmes@users.noreply.github.com>
2024-01-15 14:11:03 +0100

    Update est_verbe.c

--- est_verbe.c
+++ est_verbe.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 int est_verbe(v) {
-    if (v.type[1]=='f')
-    {    
-       return 2;
-    }
-    else
-    {
-       return 1;
-    }
+    while (v.type[1]>='a')
+        {
+            if (v.type[1]=='f')
+            {    
+               return 2;
+            }
+            else
+            {
+               return 1;
+            }
+        }
 }
