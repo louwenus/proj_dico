460a1d796c23711b1926dc80b333f98f8d37b76b
Merlode <clementdiagallo@gmail.com>
2024-01-22 12:00:38 +0100

    Ajout d'un exemple de phrase

--- ocaml/main.ml
+++ ocaml/main.ml
@@ -4,7 +4,7 @@ type pers = Je | Tu | Sing3 | Nous | Vous | Plur3 | None;;
 type classe = Nom | Verbe | Determinant | Adjectif;;
 
 type verbe = { content: string; conjugaison: pers list; };; (* S'il est à l'impératif, None sera compris dans la liste, liste vide s'il est à l'infinitif *)
-type nom = { content: string; gender: genre; count: nb }
+type nom = { content: string; gender: genre; count: nb };; (* Le string peut contenir Je/Tu/Nous/Vous qui va influencer la conjugaison *)
 type determinant = { content: string; gender: genre; count: nb };;
 type adjectif = { content: string; gender: genre; count: nb};;
 type autre = { content: string };;
@@ -14,9 +14,11 @@ type mot = Verbe of verbe | Nom of nom | Determinant of determinant | Adjectif o
 type phrase_verif = {
     (* gender_wanted: genre; // Le genre suivant de la phrase souhaité | A retirer car inutile ? *)
     gender_saw: genre; (* Le genre qui a été lu le dernier *)
-    (* nb_wanted: nb; // Pluriel qui a été lu le dernier | A retirer car inutile ?*)
+    (* nb_wanted: nb; // Pluriel qui a été lu le dernier | A retirer car inutile ? *)
     nb_saw: nb;
     last_subject: pers;
     last_verb: pers;
     is_question: bool;
 };;
+
+let (phrase: mot list) = [Nom {content = "Je"; gender = None; count = Sing;}; Verbe {content = "mange"; conjugaison = [Je; Sing3]}];;
