164ff817dd3f037c23117a905e1819e38e8d5ca1
Merlode <clementdiagallo@gmail.com>
2024-01-22 11:24:22 +0100

    Corrections de mots

--- ocaml/correction.ml
+++ ocaml/correction.ml
@@ -5,12 +5,12 @@ type pers = Je | Tu | Sing3 | Nous | Vous | Plur3 | None;;
 type verbe = { content: string; sujet: pers; };;
 type nom = { content: string; gender: genre; count: nb }
 type determinant = { content: string; gender: genre; count: nb };;
-type adjectif = { content: string: gender: genre; count: nb};;
+type adjectif = { content: string; gender: genre; count: nb};;
 
 type phrase_verif = {
-    gender_wanted: genre;
-    gender_saw: genre;
-    nb_wanted: nb;
+    (* gender_wanted: genre; // Le genre suivant de la phrase souhaité // A retirer ? *)
+    gender_saw: genre; (* Le genre qui a été lu le dernier *)
+    nb_wanted: nb; (* Pluriel qui a été lu le dernier*)
     nb_saw: nb;
     last_subject: pers;
     last_verb: pers;
